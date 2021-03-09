77488e4c9fa8ebd59986a77efaabf71a21f16555
Avijit <avijit>
2021-03-09 17:43:33 +0530

    updated form formatting and info

--- src/index.js
+++ src/index.js
@@ -7,78 +7,118 @@ app.use(bodyParser.json());
 const subscribers = new Map();
 const eventsStore = [];
 
-class EventData{
+class EventData {
 
-    constructor(eventType, eventData){
+    constructor(eventType, eventData) {
         this.time = new Date().getTime();
         this.eventType = eventType;
         this.eventData = eventData;
     }
-    toString(){
+    toString() {
         return `Event ${this.eventType} occured at ${time}`;
     }
 }
- 
-app.post('/add-subscribers', (req, res) =>{
- 
-   if(subscribers.has(req.body.type)){
+
+app.post('/add-subscribers', (req, res) => {
+
+    if (subscribers.has(req.body.type)) {
         let sub = subscribers.get(req.body.type);
         console.log('sub list');
         sub.push(req.body.subscriberAddress);
-   }else{
+    } else {
         subscribers.set(req.body.type, []);
         subscribers.get(req.body.type).push(req.body.subscriberAddress);
-   }
-   res.send('Subscriber added successfully');
+    }
+    res.send('Subscriber added successfully');
 });
 
-app.get('/list-events', (req, res) =>{
-     
-    if(subscribers.size !== 0){
+app.get('/list-events', (req, res) => {
+
+    if (subscribers.size !== 0) {
         const keys = [];
-        for (let key of subscribers.keys()){
+        for (let key of subscribers.keys()) {
             keys.push(key);
         }
-        res.send('Registered events :: '+ keys);
+        res.send('Registered events :: ' + keys);
         return;
     }
-    res.send( 'No events registered with the events bus');
+    res.send('No events registered with the events bus');
 
 });
 
-app.post('/events', (req, res) =>{
-     
-    if(req.body.type && subscribers.has(req.body.type)){
+app.post('/events', (req, res) => {
+
+    if (req.body.type && subscribers.has(req.body.type)) {
         let subscriberList = subscribers.get(req.body.type);
         stampEvent(req.body.type, req.body.data);
-        for(let subscriber of subscriberList){
+        for (let subscriber of subscriberList) {
             informSubscriber(subscriber, req.body.type, req.body.data);
-            
+
         }
         res.send('Events posted to bus');
-    } 
-    
+    }
+
 });
 
-app.get('/events', (req, res) =>{
-     
-     res.send(eventsStore);
-    
+app.get('/events', (req, res) => {
+    let message = '<ul>';
+    for (let msg of eventsStore) {
+        message += `<li>${msg}</li>`;
+    }
+    message += '</ul>';
+    res.send(`<div>Event message list </div><br/><div>${message}</div>`);
+
 });
 
- 
-const informSubscriber = async (subscriber, type, data) =>{
-    try{
-        await axios.post(subscriber, {type: type, data: data});
-    }catch(error){
-            console.log(error);
+
+app.get('/get-format', (req, res) => {
+    const postEvents = `{
+        "type": "EVENT_CREATED", 
+        "data": "abcd"
+       }`;
+
+    const postEventFormat = `Post Event Format 
+    ${postEvents};
+    `;
+
+    const addSubscribers = `
+        {
+            "type": "UPDATED", 
+            "subscriberAddress": "http://localhost:3000/send"
+           }
+    `;
+    const addSubScriberFormat = `Add subscriber Format 
+           ${addSubscribers}
+    `;
+    res.send(`<div>${postEventFormat}</div><div>${addSubScriberFormat}</div>`);
+
+});
+
+app.get('/', (req, res) => {
+
+
+    const responseString = `<div><h2>Find the list of the subscribed events at <a href="/list-events">here</a></h2></div>
+    <div><h2>Find the list of the events <a href="/events">here</a></h2></div>
+    <div><h2>To add subscriber, make  post requests to the server using the format <a href="/get-format">here</a></h2></div>
+    <div><h2>To emit events, post requests to the server using the format <a  href="/get-format">here</a></h2></div>
+    `
+    res.send(responseString);
+});
+
+
+
+const informSubscriber = async (subscriber, type, data) => {
+    try {
+        await axios.post(subscriber, { type: type, data: data });
+    } catch (error) {
+        console.log(error);
     }
 
 };
-const stampEvent = (type, data) =>{
+const stampEvent = (type, data) => {
     eventsStore.push(new EventData(type, data));
 
 }
-app.listen(3000, () =>{
-    console.log('Listening on express server !');
+app.listen(3000, () => {
+    console.log('Node express listening on 3000 !');
 });
